41d41e9dbdc6f3ad057c84108d2611dd8924929f
Artem <a.khimin@yandex.ru>
2021-11-12 12:44:59 +0300

    Test Property.Property

---
+++ CsharpNew10part2/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpNew10part2
+{
+    public class Author
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ CsharpNew10part2/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpNew10part2
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+
+        public Book(string title, Author author) =>
+            (Title ,Author)=(title, author);
+
+        public static bool doesHaveDiscount(Book book) =>
+            book switch
+            {
+                { Author: { LastName: "Richter" } }
+                or
+                {
+                    Author: { LastName: "Price" }
+                } => true, _=>false
+            };
+
+        public static bool doesHaveDiscountNew(Book book) =>
+            book switch
+            {
+                { Author.LastName: "Richter" } 
+                or
+                {
+                  Author.LastName: "Price" 
+                } => true,
+                _ => false
+            };
+
+    }
+}
+
+
+
